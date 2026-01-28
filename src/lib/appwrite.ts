07aa6bba0f03917f3da8c5c47837cbc10c7d270a
rounak695 <rounakpaul881@gmail.com>
2026-01-29 03:21:37 +0530

    chore: persist Appwrite Project ID in config

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -3,7 +3,7 @@ import { Client, Account, Databases, Storage } from 'appwrite';
 const client = new Client();
 
 const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1'; // Default or Placeholder
-const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '';
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '693d2c14001f6aca5cbc';
 
 client
     .setEndpoint(endpoint)
