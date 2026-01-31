1de9b80815cf57f8eece8288a4ee219f8ce6ec52
rounak695 <rounakpaul881@gmail.com>
2026-01-31 18:36:44 +0530

    Remove debug logs

--- src/lib/appwrite-educator.ts
+++ src/lib/appwrite-educator.ts
@@ -8,7 +8,6 @@ const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID_EDUCATOR;
 if (!projectId) {
     console.error("Appwrite Educator Project ID is missing. Please check your .env.local file.");
 } else {
-    console.log("Educator Project ID:", projectId); // Debug log
     client
         .setEndpoint(endpoint)
         .setProject(projectId);
